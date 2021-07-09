a9d614598bd09a924006ef6137eac5d6f8a066b6
eunseo2 <20180972@sungshin.ac.kr>
2021-07-09 15:58:47 +0900

    dfs

---
+++ Level2/타겟넘버.java
@@ -0,0 +1,16 @@
+public class 타겟넘버 {
+    public int solution(int[] numbers, int target) {
+        return dfs(numbers,target,0,0);
+    }
+    
+    public int dfs(int[] numbers, int target, int level, int sum){
+        if(level == numbers.length){
+            if(sum ==target) {
+                return 1;
+            }
+            else return 0;
+        }
+       
+        return dfs(numbers,target,level+1,sum+numbers[level]) + dfs(numbers, target, level+1, sum - numbers[level]);
+    }
+}
