26980c789c2e7de2a0eec62405307ab6b1c1c25b
eunseo2 <20180972@sungshin.ac.kr>
2021-07-16 12:23:15 +0900

    List 자료구조

--- Collections/ListExam.java
+++ Collections/ListExam.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -6,15 +7,23 @@ import java.util.List;
 public class ListExam {
     public static void main(String[] args){
         List<String> list = new ArrayList<>();
-        list.add("kim");
-        list.add("kang");
-        list.add("kim");
+        list.add("a");
+        list.add("d");
+        list.add("a");
 
         System.out.println(list.size());
 
         for(int i=0; i< list.size(); i++){
             System.out.println(list.get(i));
         }
+
+        //정렬
+        Collections.sort(list);
+        System.out.println("정렬"+ list);
+
+        //역순으로 정렬
+        Collections.sort(list,Collections.reverseOrder());
+        System.out.println("역순"+list);
     }
     
 }
