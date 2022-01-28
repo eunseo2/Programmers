ba2eb551f6820dfe64563d0630cc5755a7f85691
eunseo2 <20180972@sungshin.ac.kr>
2022-01-28 13:44:09 +0900

    map

--- Level2/전화번호목록.java
+++ Level2/전화번호목록.java
@@ -1,39 +1,23 @@
 import java.util.*;
 
-public class 전화번호목록 {
+class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
         
         Map<String,Integer> map = new HashMap<>();
         
-        for(int i=0; i< phone_book.length; i++)
-            map.put(phone_book[i],i);
-            
-        for(String str : phone_book){
-            for(int i = 0; i < str.length(); i++){
-                String value = str.substring(0, i);  
-                
-                if( map.containsKey( value ) ){
+        for(String phone : phone_book){
+            map.put(phone,1);
+        }
+        
+        for(String key : phone_book){
+            for(int i=1; i<key.length(); i++){
+                String str = key.substring(0,i);
+                if(map.containsKey(str)) {
+                    System.out.println(key + " " + str);
                     return false;
                 }
             }
         }
-        return answer;
+        return true;
     }
 }
-
-// import java.util.*;
-// class Solution {
-//     public boolean solution(String[] phone_book) {
-//          Arrays.sort(phone_book);
-        
-//          for(int i=0; i<phone_book.length-1; i++){
-//              for(int j=i+1; j<phone_book.length; j++){
-                 
-//                 if(phone_book[i].startsWith(phone_book[j])) {return false;} 
-//                  if(phone_book[j].startsWith(phone_book[i])) {return false;}
-//              }
-//          }
-//         return true;
-//     }
-// }
