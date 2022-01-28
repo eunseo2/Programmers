bd817fe2922097ffb3d43d1a322c43a3966d2507
eunseo2 <20180972@sungshin.ac.kr>
2022-01-28 14:50:00 +0900

    bfs

--- Level3/네트워크.java
+++ Level3/네트워크.java
@@ -1,67 +1,33 @@
 import java.util.*;
-
-public class 네트워크 {
-    static boolean[] visited;
-    
+class Solution {
+    static boolean visited[] = new boolean[201];
+    static int cnt = 0;
     
     public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n];
         
-        int answer = 0;
-        for(int i=0; i<n; i++){
+        for(int i=0; i<computers.length; i++){
             if(!visited[i]){
-                 bfs(i,computers,visited,n);
-                 answer++;
+                bfs(i,computers);
             }
-        }
-        
-        return answer;
+        }    
+        return cnt;
     }
     
-    public static void bfs(int start, int[][]computers, boolean[]visited, int n){
+    public static void bfs(int i, int[][] computers){
+        cnt++;
+        visited[i] = true;
         Queue<Integer> q = new LinkedList<>();
-        
-        q.offer(start);
-        visited[start] = true;
+        q.offer(i);
         
         while(!q.isEmpty()){
-            int x = q.poll();
-
-            for(int i=0; i<n; i++){
-                if(!visited[i] && computers[x][i] == 1 ){
-                    q.offer(i);
-                    visited[i] = true;
+            int now = q.poll();
+            
+            for(int h=0; h<computers.length; h++){
+                if(now !=h && computers[now][h] == 1 && visited[h] == false){
+                    q.offer(h);
+                    visited[h] = true;
                 }
             }
         }
-           
-    }
-    
+    } 
 }
-
-//dfs 풀이법
-// class Solution {
-    
-//     static boolean[] visited;
-//     public int solution(int n, int[][] computers) {
-//         int answer = 0;
-//         visited= new boolean[n];
-//         for(int i=0; i<n; i++){
-//             if(!visited[i]){
-//                 dfs(n,computers,i);
-//                 answer++;   
-//             }
-//         }
-//         return answer;
-//     }
-    
-//     public void dfs(int n, int[][]computers, int node){  
-//         visited[node] = true;
-        
-//         for(int i=0; i<n; i++){
-//             if(!visited[i]&& i!= node && computers[node][i]==1){
-//                 dfs(n,computers,i);
-//             }
-//         }
-//     }
-// }
