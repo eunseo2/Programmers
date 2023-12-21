b1f72e6d7cf82c316f9124939585f82421fa4329
Eunseo Kim <70589857+eunseo2@users.noreply.github.com>
2023-12-21 14:57:22 +0900

    [level 2] Title: 요격 시스템, Time: 0.44 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 1;
+        
+        Arrays.sort(targets, (o1, o2) -> o1[0] - o2[0]);
+
+		int start = targets[0][0];
+		int end = targets[0][1];
+
+		for (int i = 1; i < targets.length; i++) {
+			int nextX = targets[i][0];
+			int nextY = targets[i][1];
+
+			if (nextX < end) {
+				start = Math.max(start, nextX);
+				end = Math.min(end, nextY);
+			} else {
+				answer++;
+				start = nextX;
+				end = nextY;
+			}
+		}
+
+        
+        return answer;
+    }
+}
